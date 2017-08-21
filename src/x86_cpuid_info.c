4bf25998e10e624af9e7ea2e0c581f01da35fa24
phil@minimalmetrics.com <phil@minimalmetrics.com>
2017-08-21 06:55:45 -0500

    Added code from the virt-what package to properly detect other hypervisors. Tested on VirtualBox.

--- src/x86_cpuid_info.c
+++ src/x86_cpuid_info.c
@@ -1501,6 +1501,87 @@ init_intel( PAPI_mh_info_t * mh_info, int *levels )
   return PAPI_OK;
 }
 
+/* Returns pointer to string */
+
+/* virt-what-cpuid-helper: Are we running inside KVM or Xen HVM?
+ * Copyright (C) 2008 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* This program was suggested by Gleb Natapov and written by Paolo
+ * Bonzini, with a few modifications by Richard W.M. Jones.
+ */
+
+
+static unsigned int
+cpuid3 (unsigned int eax, char *sig)
+{
+  unsigned int *sig32 = (unsigned int *) sig;
+
+  asm volatile (
+        "xchgl %%ebx,%1; xor %%ebx,%%ebx; cpuid; xchgl %%ebx,%1"
+        : "=a" (eax), "+r" (sig32[0]), "=c" (sig32[1]), "=d" (sig32[2])
+        : "0" (eax));
+  sig[12] = 0;
+
+  return eax;
+}
+
+static char *
+vm_cpu_sig (void)
+{
+  char sig[13];
+  static char vbox[13] = "";
+  unsigned int base = 0x40000000, leaf = base;
+  unsigned int max_entries;
+
+  memset (sig, 0, sizeof sig);
+  max_entries = cpuid3 (leaf, sig);
+  if (strncmp(sig,"VBox",sizeof(vbox)) == 0) {
+    strncpy(vbox,sig,sizeof(vbox));
+    return vbox;
+  }
+
+  if (max_entries < 3 || max_entries > 0x10000) {
+    //    printf("max_entries bounds %x %s %d\n",leaf,sig,max_entries);
+    strncpy(vbox,sig,sizeof(vbox));
+    return vbox;
+  }
+  
+  /* Most hypervisors only have information in leaf 0x40000000, but
+   * upstream Xen contains further leaf entries (in particular when
+   * used with Viridian [HyperV] extensions).  CPUID is supposed to
+   * return the maximum leaf offset in %eax, so that's what we use,
+   * but only if it looks sensible.
+   */
+  if (max_entries > 3 && max_entries < 0x10000) {
+    for (leaf = base + 0x100; leaf <= base + max_entries; leaf += 0x100) {
+      memset (sig, 0, sizeof sig);
+      cpuid3 (leaf, sig);
+      if (strlen(sig)) {
+	strncpy(vbox,sig,sizeof(vbox));
+	return vbox;
+      }
+    }
+  }
+
+  return NULL;
+}
+
+/* End borrowed code  */
 
 /* Returns 1 if hypervisor detected */
 /* Returns 0 if none found.         */
@@ -1508,7 +1589,7 @@ int
 _x86_detect_hypervisor(char *vendor_name)
 {
   unsigned int eax, ebx, ecx, edx;
-  char hyper_vendor_id[13];
+  char hyper_vendor_id[13] = "";
 
   cpuid2(&eax, &ebx, &ecx, &edx,0x1,0);
   /* This is the hypervisor bit, ecx bit 31 */
@@ -1522,11 +1603,16 @@ _x86_detect_hypervisor(char *vendor_name)
     memcpy(hyper_vendor_id + 8, &edx, 4);
     hyper_vendor_id[12] = '\0';
     strncpy(vendor_name,hyper_vendor_id,PAPI_MAX_STR_LEN);
+    printf("HERE!\n");
     return 1;
-  }
-  else {
-    strncpy(vendor_name,"none",PAPI_MAX_STR_LEN);
-  }
+  } else {
+    char *r = vm_cpu_sig();
+    if (r) {
+      strncpy(vendor_name,r,PAPI_MAX_STR_LEN);
+      return 1;
+    }
+  } 
+  strncpy(vendor_name,"none",PAPI_MAX_STR_LEN);
   return 0;
 }
 
